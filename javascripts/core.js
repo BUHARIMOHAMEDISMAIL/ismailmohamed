388533834687c28f612618d1681ba0596cfcadd2
ismailmohamedbuhari <ismailmohamedbuhari@gmail.com>
2017-02-15 20:22:42 +0530

    js changes

--- javascripts/core.js
+++ javascripts/core.js
@@ -1,7 +1,9 @@
-$('.button-collapse').sideNav({
+$(document).ready(function(){
+	$('.button-collapse').sideNav({
       menuWidth: 300, // Default is 240
       closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
     }
   );
   $('.collapsible').collapsible();
   $(".button-collapse").sideNav();
+});
