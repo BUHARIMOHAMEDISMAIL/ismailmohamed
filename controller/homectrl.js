e3c4294604761e2240d0f4f6834132b2034c444d
ismailmohamedbuhari <ismailmohamedbuhari@gmail.com>
2017-02-15 20:28:57 +0530

    cher

--- controller/homectrl.js
+++ controller/homectrl.js
@@ -1,5 +1,12 @@
 mysite.controller('homectrl', function($scope, $rootScope, focus, $http) {
 	//alert("home controller is wokring good");
+	$('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 240
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    }
+  );
+  $('.collapsible').collapsible();
+  $(".button-collapse").sideNav();
 	$http.get('./javascripts/data.json').success(function(response) {
 		console.log(response);
 		$scope.post = response.post.postitems;
